14e5a1659a544aca647de5dce2e039ae0c960573
Suhas Bharadwaj <suhas.five@gmail.com>
2015-09-03 06:35:57 +0530

    Something, finally. Added social media buttons, which actually work with transitions and shit.

--- faceit/javascript/homescript.js
+++ faceit/javascript/homescript.js
@@ -7,20 +7,69 @@ $(document).ready(function() {
   // var left = document.getElementById('left');
   // left.height(heightof);
   // console.log(typeof $(window).height());
+
   var windowWidth = parseInt($(this).width());
   console.log(windowWidth);
   if (windowWidth >= 1600) {
     $("#responsive").attr("href", "css/homestyles1600.css");
+    console.log("1600");
   }
   else if (windowWidth >= 1366) {
     $("#responsive").attr("href", "css/homestyles1366.css");
+    console.log("1366");
   }
   else if (windowWidth >= 1280) {
     $("#responsive").attr("href", "css/homestyles1280.css");
+    console.log("1280");
   }
   else {
     $("#responsive").attr("href", "css/homestyles.css");
   }
+
+  $(window).click(function(event) {
+    /* Act on the event */
+    setHeight();
+  });
+
+  var socialLeft = ((((42.5 * windowWidth) / 100)) - 250)/2 - 40;
+  //socialLeft = socialLeft + "";
+  console.log(socialLeft);
+  $(".socialbuttons").css({
+    'left': socialLeft
+  });
+
+  $(".socialaddress").css({
+    'left': socialLeft + 50
+  });
+
+  // var socialTop = $(window).height() - $(this).height();
+  // $(".socialbuttons").css({
+  //    'margin-top': socialTop
+  //  });
+  $(".socialbutton").hover(function() {
+    /* Stuff to do when the mouse enters the element */
+    var id = $(this).attr("id");
+    console.log(id);
+    $(this).find('img').fadeOut('fast', function() {
+      $(this).attr('src', 'images/' + id + '.png');
+      $(this).fadeIn('fast');
+    });
+    $(this).animate({
+      param1: value1,
+      param2: value2},
+      speed, function() {
+      /* stuff to do after animation is complete */
+    });
+
+  }, function() {
+    /* Stuff to do when the mouse leaves the element */
+    var id = $(this).attr("id");
+    $(this).find('img').fadeOut('fast', function() {
+      $(this).attr('src', 'images/' + id + 'white.png');
+      $(this).fadeIn('fast');
+    });
+
+  });
 });
 
 function setHeight() {
@@ -46,15 +95,10 @@ $(document).keydown(function(event) {
   e = jQuery.Event("keydown");
   e.which = 65;
   //if(flag == "Stop") {$(document).trigger(e, "Stop" );}
-  console.log($(".lefthalf").height());
-  console.log($(window).height());
+  //console.log($(".lefthalf").height());
+  //console.log($(window).height());
   if (($(".lefthalf").height()) > (($(window).height()))) {
     setHeight();
   }
 
-});
-
-$(window).click(function(event) {
-  /* Act on the event */
-  setHeight();
 });
