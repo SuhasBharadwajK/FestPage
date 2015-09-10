6b56965eebbf782fe308268da078bcba2018b165
Suhas Bharadwaj <suhas.five@gmail.com>
2015-09-11 00:12:17 +0530

    Deleted the unnecessary crap and some necessary crap too.

--- faceit/javascript/csebranches.js
+++ faceit/javascript/csebranches.js
@@ -3,17 +3,50 @@ eventImages = Array("clang.png", "java.png", "lanparty.png");
 
 $(document).ready(function() {
 
-  $("#l1").animate({
-    marginTop: "+=100px", height: "+=400"},
-    300, "easeOutCubic", function() {
-      $(this).animate({
-        width: '+=960px'},
-        300, "easeOutCubic", function() {
-          $(this).children('span').fadeIn('400', function() {
+  $("#l1").children('span').fadeOut('fast', function() {
 
-          });
-      });
+    $("#l1").animate({
+      marginTop: "+=100px", height: "+=400"},
+      300, "easeOutCubic", function() {
+        $(this).animate({
+          width: '+=960px'},
+          300, "easeOutCubic", function() {
+            console.log("FIRRRSST");
+            $("l1").children('button').fadeIn('fast', function() {
+              console.log("faded in");
+            });
+            $("#l1").children('button').fadeIn('fast', function() {
+              console.log("faded in");
+            });
+            $(this).children('span').fadeIn('400', function() {
+
+            });
+        });
+    });
+
+
   });
+
+  // $("#l1").animate({
+  //   marginTop: "+=100px", height: "+=400"},
+  //   300, "easeOutCubic", function() {
+  //     $(this).animate({
+  //       width: '+=960px'},
+  //       300, "easeOutCubic", function() {
+  //         console.log("FIRRRSST");
+  //         $("l1").children('button').fadeIn('fast', function() {
+  //           console.log("faded in");
+  //         });
+  //         $("#l1").children('button').fadeIn('fast', function() {
+  //           console.log("faded in");
+  //         });
+  //         $(this).children('span').fadeIn('400', function() {
+  //
+  //         });
+  //     });
+  // });
+
+
 
 
   $('.events').click(eventClicked);
@@ -41,24 +74,30 @@ function revert(revertId, currentId) {
   console.log(imageUrl);
   console.log(factor);
 
-  // $("#" + revertId).children('span').fadeOut('fast', function() {
-  //
-  //
-  //
-  // });
-  $("#" + revertId).animate({
-    width: '-=960px',
-    marginLeft: toLeft},
-    200, function() {
-      $(this).animate({
-        marginTop: '-=100px', height: '-=400px'},
-        400, "easeOutCubic", function() {
-          expand(currentId);
-          // $(this).css({
-          //   'background-image': 'url("images/' + imageUrl + '")'
-          // });
-      });
+  reversing(revertId);
+
+  $("#" + revertId).children('span').fadeOut('fast', function() {
+    $("#" + revertId).animate({
+      width: '-=960px',
+      marginLeft: toLeft},
+      200, function() {
+        $(this).animate({
+          marginTop: '-=100px', height: '-=400px'},
+          400, "easeOutCubic", function() {
+
+            $(this).children('span').fadeIn('fast', function() {
+
+            });
+            expand(currentId);
+            // $(this).css({
+            //   'background-image': 'url("images/' + imageUrl + '")'
+            // });
+        });
+    });
+
+
   });
+
 
 
 }
@@ -72,17 +111,49 @@ function expand(expandId) {
     'background-image': 'none'
   });
 
-  $("#" + expandId).animate({
-    marginTop: "+=100px", height: "+=400"},
-    300, "easeOutCubic", function() {
-      $(this).animate({
-        marginLeft: toLeft,
-        width: '+=960px'},
-        400, "easeOutCubic", function() {
-          $('.events').bind('click', eventClicked);
-          $("#" + expandId).children('span').fadeIn('slow', function() {
+  $("#" + expandId).children('span').fadeOut('fast', function() {
+    $("#" + expandId).animate({
+      marginTop: "+=100px", height: "+=400"},
+      300, "easeOutCubic", function() {
+        $("#" + currentId).children('span').css({
+          //'margin-top': '20px'
+        });
+        $(this).animate({
+          marginLeft: toLeft,
+          width: '+=960px'},
+          400, "easeOutCubic", function() {
+            $('.events').bind('click', eventClicked);
+            $("#" + expandId).children('span').fadeIn('slow', function() {
+            });
+            expanded(expandId);
+        });
+    });
 
-          });
-      });
+  });
+
+  // $("#" + expandId).animate({
+  //   marginTop: "+=100px", height: "+=400"},
+  //   300, "easeOutCubic", function() {
+  //     $(this).animate({
+  //       marginLeft: toLeft,
+  //       width: '+=960px'},
+  //       400, "easeOutCubic", function() {
+  //         $('.events').bind('click', eventClicked);
+  //         $("#" + expandId).children('span').fadeIn('slow', function() {
+  //
+  //         });
+  //     });
+  // });
+}
+
+function reversing(revertId) {
+  $("#" + revertId).children('button').fadeOut('fast', function() {
+
+  });
+}
+
+function expanded(expandId) {
+  $("#" + expandId).children('button').fadeIn('fast', function() {
+
   });
 }
